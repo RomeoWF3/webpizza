188ea14981335eda7e288fd9f6649bb188789e88
Ben Haddou Aziz <benhaddouazize@gmail.com>
2019-02-20 15:07:02 +0100

    -

--- private/app/utils/getUserLanguages.php
+++ private/app/utils/getUserLanguages.php
@@ -4,9 +4,26 @@
  */
 if (!function_exists('getUserLanguages'))
 { 
-    function getUserLanguages() {
-        echo "Les langues de l'utilisateur";
+    function getUserLanguages() 
+    {
+        // On récupère la liste des langues depuis la super global $_SERVER
+       $languages_str = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
 
+       // On converti la chaine en tableau
+       $languages_arr = explode(",", $languages_str);
+
+       dump($languages_arr);
+
+
+       // On boucle sur la liste des langues
+       foreach($languages_arr as $key => $lang)
+       {
+            $lang = explode(";", $lang);
+            $lang = $lang [0];
+            dump ( $lang );
+       }
+
+        dump($languages_arr);
     }
 }
 getUserLanguages();

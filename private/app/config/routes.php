e2786704897a1b5cd8c6fd3516ad1970d90eabbb
Ben Haddou Aziz <benhaddouazize@gmail.com>
2019-02-21 17:07:04 +0100

    -

--- private/app/config/routes.php
+++ private/app/config/routes.php
@@ -15,7 +15,31 @@ $routes = [
     // Route Index (page d'accueil du site)
     ["homepage", "/", "homepage:index", ["HEAD","GET"]],
 
+    // Liste des produits
+    ["pizzas", "/pizzas", "pizzas:index", ["HEAD","GET"]],
+    ["salads", "/salades", "salads:index", ["HEAD","GET"]],
+    ["desserts", "/desserts", "desserts:index", ["HEAD","GET"]],
+    ["drinks", "/boissons", "drinks:index", ["HEAD","GET"]],
+    ["menus", "/menus", "menus:index", ["HEAD","GET"]],
+
     // Page de contact
-    ["contact", "/contact", "contact:index", ["HEAD","GET"]]
-    
+    ["contact", "/contact", "contact:index", ["HEAD","GET","POST"]],
+
+    // Pages de sécurité
+    ["login", "/login", "security:login", ["HEAD","GET","POST"]],
+    ["register", "/register", "security:register", ["HEAD","GET","POST"]],
+    ["forgotten_password", "/forgotten-password", "security:forgotten_password", ["HEAD","GET","POST"]],
+
+    // Page de commande
+    ["order", "/order", "order:index", ["HEAD","GET"]],
+
+    // Profil utilisateur
+    ["account", "/mon-compte", "account", ["HEAD","GET"]],
+
+    // ---
+
+    // Erreur 404 
+    // /!\ TOUJOURS EN DERNIER DU TABLEAU ROUTE
+    ["error-404", "/404", "errors:404", ["HEAD","GET"]]
+
 ];
